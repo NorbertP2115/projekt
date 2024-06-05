7284b8fdc9c0a9717d3317300ec62e0ebb13e901
Norbert <dsw55506@student.dsw.edu.pl>
2024-06-05 15:53:55 +0200

    Task2: Dodalem ladowanie JSON

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ def parse_args():
 if __name__ == "__main__":
     input_file, output_file = parse_args()
     print(f"Input File: {input_file}, Output File: {output_file}")
+    
+import json
+
+def load_json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except json.JSONDecodeError:
+        print("Invalid JSON format")
+        sys.exit(1)
